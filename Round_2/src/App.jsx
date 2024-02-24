7fa350c8f94331cdc37cad4fee2bd5f329319e62
Stoic Coder <shubhmehrotra2022@vitbhopal.ac.in>
2024-02-24 13:05:16 +0530

    navbar and footer

--- Round_2/src/App.jsx
+++ Round_2/src/App.jsx
@@ -1,10 +1,11 @@
-import React from "react"
+import React, { Component } from "react"
+import Navbar from "./Components/Navbar/Navbar"
 
 function App() {
 
   return (
     <>
-      
+      <Navbar />
     </>
   )
 }
